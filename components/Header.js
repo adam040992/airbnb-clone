3d69b6e72e623482298e3e9ddc0baafeb038c66f
adam040992 <adam040992@gmail.com>
2022-03-26 14:42:40 +0100

    Building: Header

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,30 @@
-import React from 'react'
+import React from 'react';
+import Image from "next/image";
 
 const Header = () => {
   return (
-    <div>
-        <h1>I am the Header</h1>
-    </div>
+    <header className='sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5'>
+        {/* Left */}
+        <div className='relative flex items-center h-10 cursor-pointer my-auto' >
+          <Image 
+            src='https://links.papareact.com/qd3'
+            layout='fill'
+            objectFit='contain'
+            objectPosition='left'
+          />
+        </div>
+
+        {/* Middle */}
+        <div>
+          <h1>Middle</h1>
+        </div>
+
+        {/* Right */}
+        <div>
+          <h1>Right</h1>
+        </div>
+
+    </header>
   )
 }
 
--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Add links to allow using items from them
+  images: {
+    domains: ['links.papareact.com']
+  }
 }
 
 module.exports = nextConfig
