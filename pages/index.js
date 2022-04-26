75798830c9251c701525400e6e86e8174497df12
adam040992 <adam040992@gmail.com>
2022-04-26 18:41:35 +0200

    Add: Footer

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Banner from '../components/Banner';
 import SmallCard from '../components/Smallcard';
 import MediumCard from '../components/MediumCard';
 import LargeCard from '../components/LargeCard';
+import Footer from '../components/Footer';
 
 export default function Home({ exploreData, cardData }) {
 
@@ -57,6 +58,8 @@ export default function Home({ exploreData, cardData }) {
         />
 
       </main>
+
+    <Footer />
 
     </div>
   )
