75aa085ef3d80240935d769363c4a490432ed8ee
adam040992 <adam040992@gmail.com>
2022-05-08 11:57:29 +0200

    Search

--- pages/search.js
+++ pages/search.js
@@ -1,8 +1,29 @@
 import React from 'react'
+import Footer from '../components/Footer'
+import Header from '../components/Header'
 
 function Search() {
   return (
-    <div>Search</div>
+    <div>
+        <Header />
+
+        <main className='flex'> 
+            <section>
+                <p className='text-xs'>300+ Stays for 5 number of guests</p>
+                <h1 className='text-3xl font-semibold mt-2 mb-6'>Stays in Mars</h1>
+
+                <div className='hidden lg:inline-flex'>
+                    <p className='px-4 py-2 border rounded-full cursor-pointer hover:shadow-lg active:scale-95 active:bg-gray-100 transition transfrom duration-100 ease-out'>Cancellation Flexibility</p>
+                    <p>Type of place</p>
+                    <p>Price</p>
+                    <p>Rooms and Beds</p>
+                    <p>More filters</p>
+                </div>
+            </section>
+        </main>
+
+        <Footer />
+    </div>
   )
 }
 
