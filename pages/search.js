c7b1d75ef17cb83db848b70a721c630ec0b61206
adam040992 <adam040992@gmail.com>
2022-05-25 17:19:41 +0200

    Add

--- pages/search.js
+++ pages/search.js
@@ -8,16 +8,16 @@ function Search() {
         <Header />
 
         <main className='flex'> 
-            <section>
+            <section className='flex-grow pt-14 px-6'>
                 <p className='text-xs'>300+ Stays for 5 number of guests</p>
                 <h1 className='text-3xl font-semibold mt-2 mb-6'>Stays in Mars</h1>
 
-                <div className='hidden lg:inline-flex'>
-                    <p className='px-4 py-2 border rounded-full cursor-pointer hover:shadow-lg active:scale-95 active:bg-gray-100 transition transfrom duration-100 ease-out'>Cancellation Flexibility</p>
-                    <p>Type of place</p>
-                    <p>Price</p>
-                    <p>Rooms and Beds</p>
-                    <p>More filters</p>
+                <div className='hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap'>
+                    <p className='button'>Cancellation Flexibility</p>
+                    <p className='button'>Type of place</p>
+                    <p className='button'>Price</p>
+                    <p className='button'>Rooms and Beds</p>
+                    <p className='button'>More filters</p>
                 </div>
             </section>
         </main>
