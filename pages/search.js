3669974616e8c2ef5fdf7f5346e32d61f1c6a25c
adam040992 <adam040992@gmail.com>
2022-05-27 15:35:49 +0200

    Pass Data

--- pages/search.js
+++ pages/search.js
@@ -2,12 +2,17 @@ import { useRouter } from 'next/dist/client/router'
 import React from 'react'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
+import { format } from 'date-fns';
 
 function Search() {
 
   const router = useRouter();
 
   const { location, startDate, endDate, noOfGuests } = router.query;
+
+  const formatedStartDate = format(new Date(startDate), 'dd MMM yyyy');
+  const formatedEndDate = format(new Date(endDate), 'dd MMM yyyy');
+  const range = `${formatedStartDate} - ${formatedEndDate}`;
 
 
   return (
@@ -16,8 +21,8 @@ function Search() {
 
         <main className='flex'> 
             <section className='flex-grow pt-14 px-6'>
-                <p className='text-xs'>300+ Stays for 5 number of guests</p>
-                <h1 className='text-3xl font-semibold mt-2 mb-6'>Stays in Mars</h1>
+                <p className='text-xs'>300+ Stays - {range} - {noOfGuests} guests</p>
+                <h1 className='text-3xl font-semibold mt-2 mb-6'>Stays in {location}</h1>
 
                 <div className='hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap'>
                     <p className='button'>Cancellation Flexibility</p>
